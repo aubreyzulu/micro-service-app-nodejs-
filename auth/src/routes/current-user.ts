e8ee50f95adaae59d77dc8a4cb64c41f9a28ea40
Aubrey Zulu <aubreydarious@gmail.com>
2022-06-18 16:28:54 +0200

    add route for getting current login user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,19 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/current-user', (req: Request, res: Response) => {
-  res.status(200).send({
-    name: 'John Doe',
-    email: '',
-  });
+  if (req.session && !req.session.jwt) {
+    return res.status(401).send({ message: 'Unauthenticated', user: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+    res.status(200).send({ user: payload });
+  } catch (error) {
+    return res.status(401).send({ message: 'Unauthenticated', user: null });
+  }
 });
 
 export { router as currentUserRouter };
