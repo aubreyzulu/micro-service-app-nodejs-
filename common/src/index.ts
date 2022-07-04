fabc637e35cdacd44378eda4808a3fa779e7af2a
Aubrey Zulu <aubreydarious@gmail.com>
2022-07-04 16:58:19 +0200

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -2,9 +2,22 @@ export type Person = {
   name: string;
   age: number;
 };
+const number = 2;
 
 function greet(person: Person) {
   return 'Hello ' + person.name;
 }
 
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = Pick<Todo, 'title' | 'completed'>;
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+};
 export default greet;
