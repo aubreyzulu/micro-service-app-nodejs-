015425689c3cd0c506f1aaecd97353e3d1db1860
Aubrey Zulu <aubreydarious@gmail.com>
2022-08-13 19:42:19 +0200

    Updates

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -5,6 +5,7 @@ export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
     id: string;
+    version: number;
     status: OrderStatus;
     userId: string;
     expiresAt: string;
